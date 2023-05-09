1329a55030ea855e708c25e28dab2555d0a00a38
amit kewal <amit.iitr.cs@gmail.com>
2023-05-10 00:48:44 +0530

    added various looging and metric features

--- PythonCode/app.py
+++ PythonCode/app.py
@@ -1,5 +1,17 @@
-from flask import Flask
+from random import randint
+from flask import Flask, request
 import os
+from opentelemetry import trace
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.sdk.trace.export import ConsoleSpanExporter
+
+provider = TracerProvider()
+processor = BatchSpanProcessor(ConsoleSpanExporter())
+provider.add_span_processor(processor)
+trace.set_tracer_provider(provider)
+tracer = trace.get_tracer(__name__)
 
 app = Flask(__name__)
 
@@ -7,6 +19,28 @@ app = Flask(__name__)
 def hello():
     return "Congrats!!!  Flask z running inside Docker!!!!"
 
+@app.route("/roll")
+def roll():
+    with tracer.start_as_current_span(
+        "server_request", 
+        attributes={ "endpoint": "/roll" } 
+    ):
+
+        sides = int(request.args.get('sides'))
+        rolls = int(request.args.get('rolls'))
+        return roll_sum(sides,rolls)
+
+def roll_sum(sides, rolls):
+    span = trace.get_current_span()
+    sum = 0
+    for r in range(0,rolls):
+        result = randint(1,sides)
+        span.add_event( "log", {
+            "roll.sides": sides,
+            "roll.result": result,
+        })
+        sum += result
+    return  str(sum)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
