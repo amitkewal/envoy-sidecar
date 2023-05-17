fd6ccc61ee72adaf4d9b8174b871a847bb6763dd
amit kewal <amit.iitr.cs@gmail.com>
2023-05-17 16:53:41 +0530

    dev

--- PythonCode/app copy.py
+++ PythonCode/app copy.py
@@ -46,3 +46,47 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(debug=True,host='0.0.0.0',port=port)
 
+
+
+############
+
+# These are the necessary import declarations
+from opentelemetry import trace
+from opentelemetry import metrics
+import os
+from random import randint
+from flask import Flask, request
+
+tracer = trace.get_tracer("diceroller.tracer")
+# Acquire a meter.
+meter = metrics.get_meter("diceroller.meter")
+
+# Now create a counter instrument to make measurements with
+roll_counter = meter.create_counter(
+    "roll_counter",
+    description="The number of rolls by roll value",
+)
+
+app = Flask(__name__)
+
+
+@app.route("/hello")
+def hello():
+    return "Congrats!!!  Flask z running inside Docker!!!!"
+
+
+@app.route("/roll")
+def roll_dice():
+    return str(do_roll())
+
+def do_roll():
+    with tracer.start_as_current_span("do_roll") as rollspan:
+        res = randint(1, 6)
+        rollspan.set_attribute("roll.value", res)
+        # This adds 1 to the counter for the given roll value
+        roll_counter.add(1, {"roll.value": res})
+        return res
+    
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True,host='0.0.0.0',port=port)
